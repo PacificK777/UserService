d07fa3f572b3723c796c0e4fc9b333a767463438
PacificK777 <prashantk0008@gmail.com>
2024-11-08 18:33:43 +0530

    added oauth2 config files

---
+++ src/main/java/com/example/userservice/Security/Repositories/ClientRepository.java
@@ -0,0 +1,12 @@
+package com.example.userservice.Security.Repositories;
+
+import com.example.userservice.Security.Models.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, String> {
+    Optional<Client> findByClientId(String clientId);
+}
